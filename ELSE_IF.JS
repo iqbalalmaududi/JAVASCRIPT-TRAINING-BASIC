deea076a778aea4c2e58848e6203ac10a06f1b8f
iqbalalmaududi <115415097+iqbalalmaududi@users.noreply.github.com>
2022-10-12 13:52:24 +0700

    Add files via upload

---
+++ ELSE_IF.JS
@@ -0,0 +1,10 @@
+let box = prompt("Masukkan cuaca : ")
+
+if (box == "hujan"){
+    console.log("Sediakan Payung")
+} else if (box == "cerah") {
+    console.log("Sediakan Kacamata Hitam")
+
+} else {
+    console.log("input salah")
+}
