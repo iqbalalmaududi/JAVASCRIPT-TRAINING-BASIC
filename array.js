ad20abec5f63e31c017f245b4a2550442674ec3c
iqbalalmaududi <115415097+iqbalalmaududi@users.noreply.github.com>
2022-10-12 13:53:54 +0700

    Add files via upload

---
+++ array.js
@@ -0,0 +1,7 @@
+//arrays
+let baris = [32, 33 , 98, 77, 21]
+//baris.push(1);
+//console.log(baris)
+/*console.log(baris.slice(0,3));*/
+/*console.log(baris.indexOf(98));*/
+console.log(baris.length)
