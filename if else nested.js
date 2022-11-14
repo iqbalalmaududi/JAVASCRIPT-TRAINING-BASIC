a9bc58cb42913c93f09922873680665bccf8ad32
iqbalalmaududi <115415097+iqbalalmaududi@users.noreply.github.com>
2022-11-14 21:47:24 +0700

    Add files via upload

---
+++ if else nested.js
@@ -0,0 +1,21 @@
+let nilai = prompt("Masukkan angka : ")
+
+if (nilai == 10){
+    if (nilai> 10){
+        if (nilai<10){
+        console.log("angka sesuai kriteria")
+        }
+        else {
+        console.log("terlalu kecil") 
+        } 
+    
+    }
+    else{
+        console.log("input terlalu besar")
+
+    }
+    
+}
+else{
+    console.log("input salah")
+}
