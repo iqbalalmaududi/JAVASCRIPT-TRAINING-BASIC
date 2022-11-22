44c65aad969ce26e16db4db4a57e7a478782a7c6
iqbalalmaududi <115415097+iqbalalmaududi@users.noreply.github.com>
2022-11-22 22:19:49 +0700

    Add files via upload

---
+++ for of generator.js
@@ -0,0 +1,13 @@
+function* generatorFunc() {
+  
+    yield 10;
+    yield 20;
+    yield 30;
+}
+
+const obj = generatorFunc();
+
+// iteration through generator
+for (let value of obj) {
+    console.log(value);
+}
